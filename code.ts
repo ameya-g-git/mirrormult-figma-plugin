7ee6a84a74dbda846495bdefc2777c1e8e4ec25f
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2023-07-17 21:53:15 -0400

    input fields worked on operating objects saved in var

--- code.ts
+++ code.ts
@@ -8,3 +8,13 @@
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
+
+figma.ui.resize(400, 500);
+
+const selectedObjects = [figma.currentPage.selection];
+figma.currentPage.selection = [];
+
+figma.ui.onmessage = async(pluginMessage) => {
+
+
+}
