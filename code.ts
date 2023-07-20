d8d6c240a314dd0287f4d84bd0d4b9e7bd7a505a
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2023-07-19 20:25:51 -0400

    fixed ts error and beginning reactive ui

--- code.ts
+++ code.ts
@@ -1,11 +1,3 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
@@ -14,7 +6,8 @@ figma.ui.resize(400, 500);
 const selectedObjects = [figma.currentPage.selection];
 figma.currentPage.selection = [];
 
+figma.ui.postMessage({mirrorObj: figma.currentPage.selection})
+
 figma.ui.onmessage = async(pluginMessage) => {
 
-
-}
+};
