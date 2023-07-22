34b2925e4df81cff79eb86c29300d62f97ca7928
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2023-07-21 22:26:23 -0400

    holding off on ts for now

--- code.ts
+++ code.ts
@@ -4,12 +4,9 @@ figma.showUI(__html__);
 figma.ui.resize(400, 500);
 
 const selectedObjects = [figma.currentPage.selection];
-const selectionRef = figma.currentPage.selection;
-
-figma.ui.postMessage({mirrorObj: selectionRef})
 
 figma.currentPage.selection = [];
 
-figma.ui.onmessage = async(pluginMessage) => {
+// figma.ui.onmessage = async(pluginMessage) => {
 
-};
+// };
