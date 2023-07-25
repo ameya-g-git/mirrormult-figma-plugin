78357cf3380114a989e58eed0be98b5d6e99fcf5
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2023-07-24 21:39:37 -0400

    made selection global

--- code.ts
+++ code.ts
@@ -3,12 +3,13 @@ figma.showUI(__html__);
 
 figma.ui.resize(400, 500);
 
-const selectedObjects = [figma.currentPage.selection];
+const selection = figma.currentPage.selection;
+
+const selectedObjects = [selection];
 
 figma.currentPage.selection = [];
 
 function getSelectedObjName() {
-    const selection = figma.currentPage.selection;
     if (selection.length > 1) {
         return "Too many objects selected!"
     }
