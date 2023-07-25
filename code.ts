e3a6f37c91815746204c9b7c9266440f862481fd
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2023-07-24 21:10:26 -0400

    fixed ts to html messaging

--- code.ts
+++ code.ts
@@ -7,6 +7,24 @@ const selectedObjects = [figma.currentPage.selection];
 
 figma.currentPage.selection = [];
 
-// figma.ui.onmessage = async(pluginMessage) => {
+function getSelectedObjName() {
+    const selection = figma.currentPage.selection;
+    if (selection.length > 1) {
+        return "Too many objects selected!"
+    }
+    else if (selection.length === 1) {
+        return selection[0].name; // Assuming you want the name of the first selected object
+    }
+    return "No object selected";
 
-// };
+};
+
+figma.on("selectionchange", () => {
+   const selectedObjName = getSelectedObjName();
+
+    figma.ui.postMessage({selectedObj: selectedObjName});
+});
+
+figma.ui.onmessage = async(pluginMessage) => {
+
+};
