bb18acb2241106e6864a0a17c8db0c63c274ce9c
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2023-08-04 23:45:04 -0400

    working on cursor generation

--- code.ts
+++ code.ts
@@ -28,24 +28,45 @@ figma.on("selectionchange", () => { // posts the name of the selected obj
     figma.ui.postMessage({selectedObj: selectedObjName});
 });
 
+
+
 figma.ui.onmessage = (pluginMessage) => {
     const msgFor = pluginMessage.msgFor;
     console.log(msgFor)
 
-    if (msgFor === 1) {
-        console.log("empty eyah")
+    // CHANGE THIS TO MAKE A COMPONENT OF THE CURSOR AND USE AN SVG FILE AS THE CURSOR InsteAD OF CREATING IT MANUALLY
 
+    if (msgFor === 1) { // empty object checkbox is checked
         const zoom = figma.viewport.zoom;
         const center = figma.viewport.center
         const size = 50; // holds a base size for the cursor at 100% zoom
-        const sizeAdjusted = size / zoom;
+        const sizeAdjusted = size / zoom; // keeps the cursor the same visual size no matter the zoom
 
-        const circle = figma.createEllipse();
-        circle.x = center.x;
-        circle.y = center.y;
-        circle.resize(sizeAdjusted, sizeAdjusted);
-        circle.fills = [{type:'SOLID', color: {r:1, g:0, b:1}}];
+        const pinkCircle = figma.createEllipse();
+        pinkCircle.x = center.x;
+        pinkCircle.y = center.y;
+        pinkCircle.resize(sizeAdjusted, sizeAdjusted);
+        pinkCircle.rotation = 135;
+        pinkCircle.name = '💖';
+        pinkCircle.fills = [{type: 'SOLID', color: {r: 1, g: 63/255, b: 190/255}}];;
+        pinkCircle.arcData = {startingAngle: 0, endingAngle: Math.PI, innerRadius: (10/12)};
 
-        console.log(circle.x, circle.y);
+        const purpCircle = figma.createEllipse();
+        purpCircle.x = center.x;
+        purpCircle.y = center.y;
+        purpCircle.resize(sizeAdjusted, sizeAdjusted);
+        purpCircle.rotation = -45;
+        purpCircle.name = '💜';
+        purpCircle.fills = [{type: 'SOLID', color: {r: 175/225, g: 11/255, b: 1}}];;
+        purpCircle.arcData = {startingAngle: 0, endingAngle: Math.PI, innerRadius: (10/12)};
+
+        const yellCircle = figma.createEllipse();
+        yellCircle.x = center.x;
+        yellCircle.y = center.y;
+        yellCircle.resize(sizeAdjusted, sizeAdjusted);
+        yellCircle.name = '💛';
+        yellCircle.fills = [{type: 'SOLID', color: {r: 1, g: 184/255, b: 0}}];
+        yellCircle.opacity = 0.8;
+        yellCircle.dashPattern = [sizeAdjusted * 8 / 39];
     }
 };
